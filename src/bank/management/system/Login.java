9a2dc3ca06bac7c5d22c00d0a1b128440e6ebaf3
chirag <chirag21r>
2025-04-08 14:18:23 +0530

    ATM PIC CHANGED

--- src/bank/management/system/Login.java
+++ src/bank/management/system/Login.java
@@ -13,6 +13,7 @@ public class Login extends JFrame implements ActionListener {
 
     JButton button1,button2,button3;
     Login(){
+
         super("Bank Management System");
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/bank.png"));
         Image i2 = i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);
@@ -28,7 +29,7 @@ public class Login extends JFrame implements ActionListener {
         iimage.setBounds(630,350,100,100);
         add(iimage);
 
-        label1 = new JLabel("WELCOME TO ATM");
+        label1 = new JLabel("WELCOME TO BANK");
         label1.setForeground(Color.WHITE);
         label1.setFont(new Font("AvantGarde", Font.BOLD, 38));
         label1.setBounds(230,125,450,40);
